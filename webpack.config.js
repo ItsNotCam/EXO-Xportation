559aa7c64401c68117a24e7ef61853e409b21889
ItsNotCam <camallanyoung@gmail.com>
2024-08-30 04:42:44 -0400

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -12,55 +12,55 @@ const htmlPlugins = templateFiles.map((filePath) => new HtmlWebpackPlugin({
 const MODE = 'development';
 
 module.exports = {
-  mode: MODE,
-  entry: './src/scripts/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'scripts.js',
-  },
-  optimization: {
-    minimize: false,
-  },
-  devtool: false,
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'src'),
-    },
-    port: 5500,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  plugins: [
-		new MiniCssExtractPlugin({filename: 'styles.css'}),
+	mode: MODE,
+	entry: './src/scripts/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'scripts.js',
+	},
+	optimization: {
+		minimize: false,
+	},
+	devtool: false,
+	devServer: {
+		static: {
+			directory: path.resolve(__dirname, 'src'),
+		},
+		port: 5500,
+		open: true,
+		hot: true,
+		compress: true,
+		historyApiFallback: true,
+	},
+	plugins: [
+		new MiniCssExtractPlugin({ filename: 'styles.css' }),
 		...htmlPlugins
 	],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: [ 
-					MiniCssExtractPlugin.loader, 
-					'css-loader', 
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				include: path.resolve(__dirname, 'src'),
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
 					'postcss-loader'
 				],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'images/[name][ext]',
-        },
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'fonts/[name][ext]',
-        },
-      },
-    ],
-  },
+			},
+			{
+				test: /\.(png|jpe?g|gif|svg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'images/[name][ext]',
+				},
+			},
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: 'asset/resource',
+				generator: {
+					filename: 'fonts/[name][ext]',
+				},
+			},
+		],
+	},
 }
