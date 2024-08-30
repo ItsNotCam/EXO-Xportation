b4ab24aa666c0421b8a09aa5cf51c5849ad63808
ItsNotCam <camallanyoung@gmail.com>
2024-08-30 03:58:12 -0400

    moved cursor into its own js file

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,5 @@
 import '../styles/styles';
-
-// Custom Cursor
-const customCursor = document.querySelector('#custom-cursor');
-document.addEventListener('mousemove', (e) => {
-	customCursor.style.left = e.clientX + 'px';
-	customCursor.style.top = e.clientY + 'px';
-
-	const hoveredElement = document.elementFromPoint(e.clientX, e.clientY);
-	if (hoveredElement != null && (hoveredElement.classList.contains('cch') || hoveredElement.closest('.cch'))) {
-		customCursor.setAttribute('data-hovered', true);
-	} else {
-		customCursor.setAttribute('data-hovered', false);
-	}
-});
+import './cursor';
 
 // SVG Button Hover Animation
 const svgButtons = document.querySelectorAll('.btn-angled');
