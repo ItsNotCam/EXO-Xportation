e488efc2b47ff6b3870370d395ab5c5ceaed587d
ItsNotCam <camallanyoung@gmail.com>
2024-08-30 15:42:54 -0400

    replaced tabs with spaces

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,5 +7,5 @@ import './components/exo-cursor';
 
 // remove the index html from the url because of how relative pathing works with links lol
 if (window.location.pathname.endsWith("index.html")) {
-	window.history.replaceState({}, document.title, window.location.pathname.replace("index.html", ""));
+  window.history.replaceState({}, document.title, window.location.pathname.replace("index.html", ""));
 }
