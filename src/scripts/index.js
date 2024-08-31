6a5e2a123e3b658957027a68e16c9074ebc2442f
ItsNotCam <camallanyoung@gmail.com>
2024-08-30 20:52:46 -0400

    buttons now scale with window width

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,3 +9,27 @@ import './components/exo-cursor';
 if (window.location.pathname.endsWith("index.html")) {
   window.history.replaceState({}, document.title, window.location.pathname.replace("index.html", ""));
 }
+
+const updateBtnScale = (buttonElement, baseWidth) => {
+	const percent = Math.min(1, window.innerWidth / 2560.0);
+	const value = parseFloat(lerp(0.7, 1, percent));
+	buttonElement.style.transform = `scale(${value})`;
+
+	const width = parseFloat(baseWidth * value);
+	buttonElement.style.width = `${width}px`;
+}
+
+const lerp = (a, b, n) => {
+	return (1 - n) * a + n * b;
+}
+
+const angledBtns = document.querySelectorAll('exo-link-btn');
+// angledBtns = [...angledBtns, ...document.querySelectorAll('exo-btn')];
+
+if(angledBtns.length > 0) {
+	const baseWidth = window.getComputedStyle(angledBtns[0]).width.replace("px", "");
+	angledBtns.forEach((btn) => updateBtnScale(btn, baseWidth));
+	window.addEventListener('resize', () => {
+		angledBtns.forEach((btn) => updateBtnScale(btn, baseWidth));
+	});
+}
