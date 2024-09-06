549ea112b887f7394c0db12da551db5f2d2ce065
ItsNotCam <camallanyoung@gmail.com>
2024-09-06 16:59:38 -0400

    added more custom properties to tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     './src/**/**.html', 
@@ -6,12 +8,36 @@ module.exports = {
   ],
   theme: {
     extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				md: '0 0.05em 0.06em var(--tw-shadow-color)',
+        lg: '0 0.037em 0.046em var(--tw-shadow-color)',
+      },
+			fontSize: {
+        'em-0.1': '0.1em',
+        'em-0.2': '0.2em',
+        'em-0.3': '0.3em',
+        'em-0.4': '0.4em',
+        'em-0.5': '0.5em',
+        'em-0.6': '0.6em',
+        'em-0.7': '0.7em',
+        'em-0.8': '0.8em',
+        'em-0.9': '0.9em',
+        'em-1.0': '1em',
+        'em-1.1': '1.1em',
+        'em-1.2': '1.2em',
+        'em-1.3': '1.3em',
+        'em-1.4': '1.4em',
+        'em-1.5': '1.5em'
+			},
       screens: {
         '720p': {'max': '1280px'},
         'home-lg': {'max': '1140px'},
         'home-md': {'max': '645px'},
         'home-sm': {'max': '450px'},
-        'home-xs': {'max': '428px'}
+        'home-xs': {'max': '428px'},
+        "nav-fold-in": {"max": "960px"}
       },
       lineHeight: {
         'em-0.1': '0.1em',
@@ -30,9 +56,6 @@ module.exports = {
         'em-1.4': '1.4em',
         'em-1.5': '1.5em'
       },
-      screens: {
-        "nav-fold-in": "960px"
-      },
       dropShadow: {
         "custom-lg": "2px 2px 2px rgba(0,0,0,0.4)",
         "custom-sm": "1px 1px 1px rgba(0,0,0,0.5)"
@@ -49,7 +72,7 @@ module.exports = {
         "9/10": "90%",
       },
       colors: {
-        custom: {
+        exo: {
           light: {
             100: "#FFFFFF",
             200: "#E0E0E0",
@@ -64,5 +87,16 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 };
