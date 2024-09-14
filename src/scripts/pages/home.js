bf0299077c9ab057e137e41e1798a68661148dd4
ItsNotCam <camallanyoung@gmail.com>
2024-09-13 20:07:10 -0400

    disclaimer statement

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,1 +1,13 @@
 import '../../styles/pages/home.css';
+import $ from 'jquery';
+
+$(function() {
+	const disclaimer = $("#disclaimer");
+	$("#open-disclaimer-btn").on("click", function() {
+		disclaimer.attr("data-visible", "true");
+	});
+
+	$("#close-disclaimer-btn").on("click", function() {
+		disclaimer.attr("data-visible", "false");
+	});
+});
