1b3dab45043bceb60d2ad75f42857cd50f6c8bba
ItsNotCam <camallanyoung@gmail.com>
2024-09-17 12:19:20 -0400

    Refactor book.js and add ExoBookFlightItem component

--- src/scripts/pages/book.js
+++ src/scripts/pages/book.js
@@ -1,1 +1,50 @@
 import '../../styles/pages/book.css';
+import './book/exo-book-flight-item';
+import $ from 'jquery';
+
+const form = $("#book-flight-form");
+const flights = $("exo-book-flight-item")
+
+const pageState = {
+	"book-fligh-form-open": false,
+	"selected-planet-id": "europa",
+	"selected-flight-id": undefined
+}
+
+// watch for page changes
+const pageStateHandler = {
+	set: function(obj, prop, value) {
+		obj[prop] = value;
+		console.log(`pageState changed: ${prop} = ${value}`);
+
+		// open / close the flight form
+		form.attr("data-visible", pageState["book-flight-form-open"]);
+		$("#nav-container").css("z-index", pageState["book-flight-form-open"] ? "0" : "50")
+
+		// set the selected flight
+		flights.each(function() {
+			const isSelectedId = this.id === pageState["selected-flight-id"];
+			const closestSelectFlightBtn = $(this).find(".select-flight");
+			$(this).attr("data-selected", isSelectedId);
+			closestSelectFlightBtn.attr("data-light", isSelectedId);
+			if(isSelectedId) {
+				closestSelectFlightBtn.attr("data-light", "true");
+				closestSelectFlightBtn.attr("data-title", "SELECTED");
+			} else {
+				closestSelectFlightBtn.removeAttr("data-light");
+				closestSelectFlightBtn.attr("data-title", "SELECT");
+			}
+		});
+
+		return true;
+	}
+};
+
+const observedPageState = new Proxy(pageState, pageStateHandler);
+
+$("#book-open").on("click", () => observedPageState["book-flight-form-open"] = true);
+$("#book-close").on("click", () => observedPageState["book-flight-form-open"] = false);
+$(".select-flight").each(function() {
+	const closestFlightID = $(this).closest(".book-item-flight").attr("id");
+	$(this).on("click", () => observedPageState["selected-flight-id"] = closestFlightID);
+})
---
+++ src/scripts/pages/book/exo-book-flight-item.js
@@ -0,0 +1,63 @@
+class ExoBookFlightItem extends HTMLElement {
+  constructor() {
+    super();
+
+    const id = this.id;
+    const company = this.querySelector('[slot="company"]').innerHTML;
+    const method = this.querySelector('[slot="method"]').innerHTML;
+    const startTime = this.querySelector('[slot="start-time"]').innerHTML;
+    const startDate = this.querySelector('[slot="start-date"]').innerHTML;
+    const endTime = this.querySelector('[slot="end-time"]').innerHTML;
+    const endDate = this.querySelector('[slot="end-date"]').innerHTML;
+    const startLocation = this.querySelector('[slot="start-location"]').innerHTML;
+    const endLocation = this.querySelector('[slot="end-location"]').innerHTML;
+    const startAirport = this.querySelector('[slot="start-airport"]').innerHTML;
+    const endAirport = this.querySelector('[slot="end-airport"]').innerHTML;
+    const startPlanet = this.querySelector('[slot="start-planet"]').innerHTML;
+    const endPlanet = this.querySelector('[slot="end-planet"]').innerHTML;
+    const cost = this.querySelector('[slot="cost"]').innerHTML;
+    const travellerAmount = this.querySelector('[slot="traveller-amount"]').innerHTML;
+		const selected = this.hasAttribute("data-selected") && this.getAttribute("data-selected") === "true";
+
+    this.innerHTML = /*html*/ `
+			<div id="${id}" class="book-item-flight">
+				<div class="flex justify-between items-center [&>h1]:text-2xl">
+					<h1>${company}</h1>
+					<h1>${method}</h1>
+				</div>
+				<div class="flex flex-col w-full">
+					<div class="w-full">
+						<span class="font-bold">${startTime}</span>
+						<span>${startDate}</span>
+						-
+						<span class="font-bold">${endTime}</span>
+						<span>${endDate}</span>
+					</div>
+					<div class="w-full text-xl uppercase">
+						<span class="font-bold">${startLocation}</span>
+						<span>${startAirport}</span>
+						-
+						<span class="font-bold">${endLocation}</span>
+						<span>${endAirport}</span>
+					</div>
+					<div class="w-full">
+						<span>${startPlanet}</span>
+						<span>${endPlanet}</span>
+					</div>
+				</div>
+				<div class="flex flex-row justify-between w-full mt-auto items-center">
+					<button 
+						class="select-flight btn h-[2.5rem] w-[7rem]" 
+						data-title="SELECT"
+					></button>
+					<div class="text-right">
+						<p class="font-bold text-2xl">${cost}</p>
+						<p>${travellerAmount}</p>
+					</div>
+				</div>
+			</div>
+		`;
+  }
+}
+
+customElements.define("exo-book-flight-item", ExoBookFlightItem);
