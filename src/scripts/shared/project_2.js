74e36f3c932e5bc9fda6a45b852b746908e7b228
ItsNotCam <camallanyoung@gmail.com>
2024-10-08 14:48:43 -0400

    replaced tabs with spaces

--- src/scripts/shared/project_2.js
+++ src/scripts/shared/project_2.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 // Create the form element with jQuery
 const transitionDurationMs = 200;
 const form = $("<form>", {
-	class: `
+  class: `
 		z-[1000]
 		fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
 		place-items-center
@@ -11,14 +11,14 @@ const form = $("<form>", {
 		bg-white/1 backdrop-blur-sm text-center 
 		transition-opacity
 	`,
-	css: {
-		display: "grid",
-		transitionDuration: `${transitionDurationMs}ms`,
-		opacity: 0,
-		pointerEvents: "none"
-	},
-	"data-visible": "false",
-	html: /* html */ `
+  css: {
+    display: "grid",
+    transitionDuration: `${transitionDurationMs}ms`,
+    opacity: 0,
+    pointerEvents: "none",
+  },
+  "data-visible": "false",
+  html: /* html */ `
 	<div class="flex flex-col justify-start border border-exo-light-100 p-4 bg-exo-dark-500/75">
 		<label for="username" class="text-left">Please enter your name</label>
 		<input type="text" autocomplete="off" class="
@@ -30,21 +30,20 @@ const form = $("<form>", {
 		</button>
 	</div>
 	`,
-	appendTo: "body"
+  appendTo: "body",
 });
 
 // handle form submission event to change the display name
 form.on("submit", (e) => {
-	e.preventDefault(); // Prevent the form from
-	const username = $("#username"); // Get the username element
-	if(username.val().length > 0) {
-		setDisplayName(username.val()); // Set the display name with the input value
-		setFormVisible(false); // Hide the form
-	} else {
-		alert("No name was given"); // Alert the user if no name was provided
-	}
-})
-
+  e.preventDefault(); // Prevent the form from
+  const username = $("#username"); // Get the username element
+  if (username.val().length > 0) {
+    setDisplayName(username.val()); // Set the display name with the input value
+    setFormVisible(false); // Hide the form
+  } else {
+    alert("No name was given"); // Alert the user if no name was provided
+  }
+});
 
 /**
  * Sets the visibility of a form element by adjusting its CSS properties, then hiding it
@@ -52,33 +51,29 @@ form.on("submit", (e) => {
  * @param {boolean} visible - A boolean indicating whether the form should be visible.
  */
 const setFormVisible = (visible) => {
-	// set the visibility of the form
-	form.css({
-		"display": "grid",
-		"opacity": visible ? "100%" : "0",
-		"pointerEvents": visible ? "auto" : "none"
-	})
+  // set the visibility of the form
+  form.css({
+    display: "grid",
+    opacity: visible ? "100%" : "0",
+    pointerEvents: visible ? "auto" : "none",
+  });
 
-	if(!visible) {
-		setTimeout(() => 
-			form.css({"display": "none"}), transitionDurationMs
-		); // set its diaplay to none after the opacity transition duration has elapsed
-	} else {
-		// set the user to focus and select all text within the element
-		$("#username")
-			.trigger("focus")
-			.trigger("select");
+  if (!visible) {
+    setTimeout(() => form.css({ display: "none" }), transitionDurationMs); // set its diaplay to none after the opacity transition duration has elapsed
+  } else {
+    // set the user to focus and select all text within the element
+    $("#username").trigger("focus").trigger("select");
 
-			const exoName = localStorage.getItem("exo-name");
-			if (exoName !== null && exoName.length > 0) {
-				$(document).on("keydown", (e) => {
-					if (e.key === "Escape") {
-						setFormVisible(false);
-					}
-				});
-			}
-	}
-}
+    const exoName = localStorage.getItem("exo-name");
+    if (exoName !== null && exoName.length > 0) {
+      $(document).on("keydown", (e) => {
+        if (e.key === "Escape") {
+          setFormVisible(false);
+        }
+      });
+    }
+  }
+};
 
 /**
  * Sets the display name in the local storage
@@ -86,87 +81,80 @@ const setFormVisible = (visible) => {
  * @param {string} name - The name to be set as the display name.
  */
 const setDisplayName = (name) => {
-	// Set the name in local storage
-	localStorage.setItem("exo-name", name);
+  // Set the name in local storage
+  localStorage.setItem("exo-name", name);
 
-	// Update each of the elements that display the welcome message
-	$(".display-name").each(function() {
-		// Find the span with class "username" and set its text to the inputted name
-		$(this).find(".username").html(/* html */ `
+  // Update each of the elements that display the welcome message
+  $(".display-name").each(function () {
+    // Find the span with class "username" and set its text to the inputted name
+    $(this).find(".username").html(/* html */ `
 			<span class="text-base">Welcome,</span>
 			<span class="username text-exo-light-100 text-lg"> ${name}</span>
 		`);
-	});
+  });
 };
-
-
 
 /**************************/
 /* ASSIGNMENT REQUIREMENT */
 /*  FULFILLMENT SECTION   */
 /**************************/
 
-
 // JQuery function to wait for the entire page to load
-$(function() {
-	/*****************/
-	/* RANDOM IMAGES */
-	/*****************/
-	// get the last opened image so we dont repeat the same image twice
-	let lastRngImage = localStorage.getItem("exo-last-rng-image");
-	if (lastRngImage === null) {
-		lastRngImage = 0;
-	} else {
-		lastRngImage = parseInt(lastRngImage);
-	}
-	let randomImgIdx;
-	do {
-		randomImgIdx = Math.floor(Math.random() * 3);
-	} while (randomImgIdx === lastRngImage);
+$(function () {
+  /*****************/
+  /* RANDOM IMAGES */
+  /*****************/
+  // get the last opened image so we dont repeat the same image twice
+  let lastRngImage = localStorage.getItem("exo-last-rng-image");
+  if (lastRngImage === null) {
+    lastRngImage = 0;
+  } else {
+    lastRngImage = parseInt(lastRngImage);
+  }
+  let randomImgIdx;
+  do {
+    randomImgIdx = Math.floor(Math.random() * 3);
+  } while (randomImgIdx === lastRngImage);
 
-	localStorage.setItem("exo-last-rng-image", randomImgIdx);
+  localStorage.setItem("exo-last-rng-image", randomImgIdx);
 
-	// set the image
-	$(".exo-astro-bg").each(function(index) {
-		console.log(randomImgIdx, index)
-		$(this).css({ display: randomImgIdx === index ? "block" : "none" })
-	})
+  // set the image
+  $(".exo-astro-bg").each(function (index) {
+    console.log(randomImgIdx, index);
+    $(this).css({ display: randomImgIdx === index ? "block" : "none" });
+  });
 
+  /*************************************/
+  /* SET THE DISPLAY NAME FROM STORAGE */
+  /* OR SHOW DISPLAY NAME INPUT FORM   */
+  /*************************************/
+  const exoName = localStorage.getItem("exo-name"); // get the name from the local storage
+  if (exoName !== null && exoName.length > 0) {
+    setDisplayName(exoName); // set the display name
+    setFormVisible(false); // hide the name editing form
+    form.find("input[type='text']").val(exoName); // set the form's text to the current name
+  } else {
+    setFormVisible(true); // show the name editing form
+  }
 
-	/*************************************/
-	/* SET THE DISPLAY NAME FROM STORAGE */
-	/* OR SHOW DISPLAY NAME INPUT FORM   */
-	/*************************************/
-	const exoName = localStorage.getItem("exo-name");	// get the name from the local storage
-	if (exoName !== null && exoName.length > 0) {
-		setDisplayName(exoName); // set the display name 
-		setFormVisible(false);   // hide the name editing form
-		form.find("input[type='text']").val(exoName); // set the form's text to the current name
-	} else {
-		setFormVisible(true); // show the name editing form
-	}
+  // Each of these buttons will show the name editing form
+  $(".display-name")
+    .find("button")
+    .each(function () {
+      $(this).on("click", function () {
+        setFormVisible(true);
+      });
+    });
 
-	// Each of these buttons will show the name editing form
-	$(".display-name").find("button").each(function() {
-		$(this).on("click", function() {
-			setFormVisible(true);
-		})}
-	);
+  /**************************/
+  /* SET LAST TIME MODIFIED */
+  /**************************/
+  $("#last-modified").text(`Last Updated: ${document.lastModified}`);
 
-
-	/**************************/
-	/* SET LAST TIME MODIFIED */
-	/**************************/
-	$("#last-modified").text(`Last Updated: ${document.lastModified}`)
-
-
-	/**********************/
-	/* OPEN A NEW WINDOW  */
-	/**********************/
-	$("#open-window-btn").on("click", function() {
-		window.open(
-			$(this).attr("href"), 
-			$(this).attr("target")
-		);
-	})
-})
+  /**********************/
+  /* OPEN A NEW WINDOW  */
+  /**********************/
+  $("#open-window-btn").on("click", function () {
+    window.open($(this).attr("href"), $(this).attr("target"));
+  });
+});
